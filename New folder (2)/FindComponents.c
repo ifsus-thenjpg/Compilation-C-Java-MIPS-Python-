c0cb22ff7566fd322c92b93058dc590c410431d2
Susanna Morin <susanna.morin@gmail.com>
2019-05-11 13:26:03 -0700

    Add files via upload

---
+++ New folder (2)/FindComponents.c
@@ -0,0 +1,129 @@
+//Susanna Morin
+//sumorin
+//pa5
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include "Graph.h"
+
+#define MAX_LEN 160
+
+int main(int argc, char * argv[])
+{
+
+    FILE *in, *out;
+    char line[MAX_LEN]; //where each line is read
+    int u,v,n,sentinel = 0;
+    Graph G = NULL;
+    Graph T = NULL;
+    List S;
+    List L;
+
+    // check command line for 2 command line arguments :
+    // if two args: in and out are not specified program with - unsuccessfull termination
+    if( argc != 3 )
+    {
+        printf("Usage: %s <input file> <output file>\n", argv[0]);
+        exit(1);
+    }
+
+    // open files for reading and writing
+    in = fopen(argv[1], "r");
+    out = fopen(argv[2], "w");
+
+    //checks for invalid input file, if null exits program with - unsuccessfull termination
+    if( in==NULL )
+    {
+        printf("Unable to open file %s for reading\n", argv[1]);
+        exit(1);
+    }
+
+    //checks for invalid output file, if null exits program with - unsuccessfull termination
+    if( out==NULL )
+    {
+        printf("Unable to open file %s for writing\n", argv[2]);
+        exit(1);
+    }
+
+    int counter1 = 1;
+    while(fgets(line, MAX_LEN, in) != NULL)
+    {
+        if(counter1 == 1)
+        {
+            //initialize a new graph of order n
+            sscanf(line," %d", &n);
+            G = newGraph(n);
+            counter1++;
+        }
+        else
+        {
+            //scan line and parse it into two ints
+            sscanf(line," %d %d", &u, &v);
+            //check to see that we are still in pt 1 of input
+            if(sentinel != v && sentinel != u)
+            {
+                //since we are still in pt 1 of input - add a directed edge from u to v
+                addArc(G,u,v);
+            }
+              
+         }
+       
+     }
+  
+    //done processing the file, so we print adjacency list 
+        printGraph(out, G);
+         
+     //create a list that will be in vector label sorted order {1,2,3...n} to feed into DFS
+         S = newList();
+         for(int i = 1; i <= getOrder(G);i++ ){
+           append(S, i);
+         }
+//          printList(out, S);
+    //start computing the strongly connected components of G
+         DFS(G, S); //run DFS the first time on the adj list in vector label sorted order 
+//          fprintf(out, "here is the list after first run of DFS");
+//          printList(out, S);
+         T = transpose(G);
+//          printGraph(out, T); ///////test 
+         DFS(T, S); //run DFS the second time on the stack list from first run of DFS, the stack list from this run will be topological sorted order 
+        
+//         fprintf(out, "here is the list after second run of DFS");
+//         printList(out, S);
+   //now we start printing the connected components of G using the stack using List ADT
+        int count =0;
+        for(moveFront(S); index(S) != -1; moveNext(S)){
+         int check = get(S);
+          if(getParent(T, check) == NIL){
+            count++;
+          }
+        }
+        
+        fprintf(out, "\nG contains %d strongly connected components: \n", count);
+        int i = 0;
+        L = newList(); //create a list to hold the strongly connected component(s)
+        for (moveBack(S); index(S)>=0; movePrev(S)){
+        //start from the back of the list representing the bottom of stack 
+          //read vertices until one has a NIL as parent, then print 
+           int v = get(S);
+           prepend(L, v);
+            if(getParent(T, v) == NIL){
+              i++; //increment the number of SCC
+              fprintf(out, "Component %d: ", i);
+              printList(out, L);
+              fprintf(out, "\n");
+              clear(L); //clear L so we can reuse it for next SCC          
+            }          
+        }
+    //free memory
+
+    freeGraph(&T);
+    freeList(&L);
+    freeList(&S);
+    freeGraph(&G);
+//close the input/output files
+    fclose(in);
+    fclose(out);
+
+}
---
+++ New folder (2)/List.h
@@ -0,0 +1,82 @@
+//Susanna Morin
+//sumorin
+//pa4
+#ifndef LIST_H
+#define LIST_H
+
+// Exported Types --------------------------------------------------------------
+typedef struct ListObj* List;
+
+// Constructors-Destructors ---------------------------------------------------
+List newList(void);
+//Constructor
+//Creates a new empty list.
+void freeList(List *pL);
+// frees all heap memory associated with its List* argument,
+// and sets *pL to NULL
+// Access functions -----------------------------------------------------------
+int length(List L);
+// Access functions
+// Returns the number of elements in this List
+int index(List L);
+// If cursor is defined, returns the index of the cursor element,
+// otherwise returns -1.
+int front(List L);
+ // Returns front element. Pre: length()>0
+int back(List L);
+// Returns back element. Pre: length()>0
+int get(List L);
+// Returns cursor element. Pre: length()>0, index()>=0
+int equals(List A, List B);
+// Returns true if and only if this List and L are the same
+// integer sequence. The states of the cursors in the two Lists
+// are not used in determining equality.
+// Manipulation procedures ----------------------------------------------------
+void clear(List L);
+// Resets this List to its original empty state.
+void moveFront(List L);
+// If List is non-empty, places the cursor under the front element,
+// otherwise does nothing.
+void moveBack(List L);
+// If List is non-empty, places the cursor under the back element,
+// otherwise does nothing.
+void movePrev(List L);
+// If cursor is defined and not at front, moves cursor one step toward
+// front of this List, if cursor is defined and at front, cursor becomes
+// undefined, if cursor is undefined does nothing.
+void moveNext(List L);
+// If cursor is defined and not at back, moves cursor one step toward
+// back of this List, if cursor is defined and at back, cursor becomes
+// undefined, if cursor is undefined does nothing.
+void prepend(List L, int data);
+// Insert new element into this List. If List is non-empty,
+// insertion takes place before front element.
+void append(List L, int data);
+// Insert new element into this List. If List is non-empty,
+    // insertion takes place after back element.
+void insertBefore(List L, int data);
+// Insert new element before cursor.
+// Pre: length()>0, index()>=0
+void insertAfter(List L, int data);
+// Inserts new element after cursor.
+// Pre: length()>0, index()>=0
+void deleteFront(List L);
+ // Deletes the front element. Pre: length()>0
+void deleteBack(List L);
+// Deletes the back element. Pre: length()>0
+void delete(List L);
+// Deletes cursor element, making cursor undefined.
+// Pre: length()>0, index()>=0
+//delete current
+void setIndex(List L, int index);
+// Other operations -----------------------------------------------------------
+void printList(FILE* out, List L);
+// prints the L to the file pointed to by out, formatted as a
+// space-separated string.
+void printListStdOut(List L);
+//prints a list to std out
+List copyList(List L);
+// Returns a new List representing the same integer sequence as this
+// List.  The cursor in the new list is undefined, regardless of the
+// state of the cursor in this List. This List is unchanged.
+#endif
